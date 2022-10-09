00af9d8d9e1d3230c033d2ff0354df41b9b077e0
Akki <87625749+akansha-minhas@users.noreply.github.com>
2022-10-09 13:00:43 -0600

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,17 @@
+import random
+response = "y"
+no = random.randint(1,6)
+
+
+response=(input())
+if response == y:
+    while response == "y":
+        if no == 1:
+            print("[-----]")
+            print("[     ]")
+            print("[  0  ]")
+            print("[-----]")
+        input("Enter y to continue and n to quit")
+    else: 
+        quit()
+
